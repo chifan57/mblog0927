874f1384bc728ef71404ac6f08e372f94eb3e3a3
chifan <11056014@ntub.edu.tw>
2023-10-25 14:30:10 +0800

    1025

--- mysite/admin.py
+++ mysite/admin.py
@@ -4,4 +4,5 @@ from mysite.models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title','slug','pub_date')
-admin.site.register(Post,PostAdmin)
+    
+admin.site.register(Post, PostAdmin)
--- mysite/models.py
+++ mysite/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length = 200)
-    slug = models.CharField(max_length = 200)
-    body = models.TextField()#不限制欄位
-    pub_date = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        ordering = ('-pub_date', ) 
+        ordering = ('-pub_date', )
         
     def __str__(self) -> str:
         return self.title
-    
